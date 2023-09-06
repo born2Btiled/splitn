b4463b367a6920648c2fa5a7e0900804f06758e6
Michał Warzocha <79743707+born2Btiled@users.noreply.github.com>
2023-09-06 11:46:03 +0200

    add pattern option (#4)

--- splitn/main.py
+++ splitn/main.py
@@ -1,5 +1,6 @@
 import typer
 from typing_extensions import Annotated
+from typing import Optional
 
 from os import path
 from re import fullmatch
@@ -14,17 +15,18 @@ def generate_output(
     operand: str,
     separator: str,
     times: int,
-    as_string: bool
+    as_string: bool,
+    patterns: list[str] | None
 ) -> None:
     try:
         if as_string or detect_string(operand):
             # handle simple strings
-            generate_splitted_sequences(operand, separator)
+            generate_splitted_sequences(operand, separator, patterns)
         else: 
             # handle regular expressions
             for counter in range(times):
                 sequence = random_sequence(operand)
-                generate_splitted_sequences(sequence, separator)
+                generate_splitted_sequences(sequence, separator, patterns)
                 if counter < times - 1:
                     print()
     except Exception as e:
@@ -33,10 +35,18 @@ def generate_output(
 @logger.catch
 def generate_splitted_sequences(
     sequence: str,
-    separator: str
+    separator: str,
+    patterns: list[str] | None
 ) -> None:
     for splitted_sequence in splitted(sequence, separator):
-        print(splitted_sequence)
+        printable: bool = False if patterns else True
+        if patterns:
+            for pattern in patterns:
+                printable = fullmatch(pattern, splitted_sequence.strip())
+                if printable:
+                    break
+        if printable:
+            print(splitted_sequence)
 
 @logger.catch
 def detect_string(
@@ -51,7 +61,7 @@ app = typer.Typer()
 
 @app.command()
 def main(
-    operands: Annotated[list[str], typer.Argument(
+    operands: Annotated[Optional[list[str]], typer.Argument(
         help="""
         List of strings, regular expressions or files.
 
@@ -61,7 +71,7 @@ def main(
 
         Given operands are treated as regular expressions by default.
         """
-    )],
+    )] = None,
     separator: Annotated[str, typer.Option(
         "--separator", "-s",
         help="Separator used in splitting generated sequences."
@@ -77,20 +87,34 @@ def main(
     as_string: Annotated[bool, typer.Option(
         "--as-string",
         help="Interpret provided operands as simple strings."
-    )] = False
-):
-    for operand, counter in zip(operands, range(len(operands), 0, -1)):
-        if path.exists(operand):
-            with open(operand) as file:
-                lines = file.readlines()
-                for line, line_counter in zip(lines, range(len(lines), 0, -1)):
-                    generate_output(line.strip(), separator, times, as_string)
-                    if line_counter > 1:
-                        print(secondary_separator)
-        else:
-            generate_output(operand, separator, times, as_string)
+    )] = False,
+    patterns: Annotated[list[str], typer.Option(
+        "--pattern", "-p",
+        help="Use this option to either generate random sequence from regular expressions without splitting, or to narrow down sequences generated from given operands to those matching provided regular expressions.",
+    )] = None
+) -> None:
+    if operands:
+        for operand, counter in zip(operands, range(len(operands), 0, -1)):
+            if path.exists(operand):
+                with open(operand) as file:
+                    lines = file.readlines()
+                    for line, line_counter in zip(lines, range(len(lines), 0, -1)):
+                        generate_output(line.strip(), separator, times, as_string, patterns)
+                        if line_counter > 1:
+                            print(secondary_separator)
+            else:
+                generate_output(operand, separator, times, as_string, patterns)
+            if counter > 1:
+                print(secondary_separator)
+    elif patterns:
+        for pattern, counter in zip(patterns, range(len(patterns), 0, -1)):
+            print(random_sequence(pattern))
         if counter > 1:
             print(secondary_separator)
+    else:
+        print("Missing argument 'OPERANDS...' or an option.")
+        raise typer.Exit(code=1)
+
 
 if __name__ == "__main__":
     app()
