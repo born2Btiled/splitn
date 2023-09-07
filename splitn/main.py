39006fff81121e4af9773f2cc97052150d27023c
Michał Warzocha <79743707+born2Btiled@users.noreply.github.com>
2023-09-07 15:14:39 +0200

    Use argparse (#6)
    * fix pattern option to be usable with times option
    
    * use argparse
    
    * expand README
    
    * update version
    
    * fix missing function name
    
    * fix parser placing in script

--- splitn/main.py
+++ splitn/main.py
@@ -1,14 +1,145 @@
-import typer
-from typing_extensions import Annotated
-from typing import Optional
-
+import argparse
+from sys import argv
 from os import path
 from re import fullmatch
+
+from rich.console import Console
+from rich.panel import Panel
+from rich.table import Table
 
 from loguru import logger
 
 from utils.sequences import random_sequence
 from utils.split import split_sequences
+
+VERSION = "3.2.0"
+
+DESCRIPTION = """
+[underline]splitn[/underline] - a CLI application that generates combinations of chars being a result of splitting strings
+"""
+
+USAGE = """
+[yellow]Usage:[/yellow]  [b]splitn \\[options] \\[strings... | regexes... | files...]
+        splitn \\[options] \\[strings... | regexes... | files...] --pattern | -p <regexes...>
+        splitn \\[--times <integer>] \\[--secondary-separator <string>] --pattern | -p <regexes...>[/b]"""
+
+EXAMPLE = """
+$ splitn abc "\\d{2}"
+abc
+a bc
+ab c
+a b c
+---
+60
+6 0"""
+
+EPILOG = "For options, use 'splitn --help'."
+
+ARGUMENTS = Table(box=None)
+ARGUMENTS.add_column(justify="left")
+ARGUMENTS.add_column(justify="left", style="yellow")
+ARGUMENTS.add_column(justify="left")
+ARGUMENTS.add_row(
+    "operands",
+    "\\[strings... | regexes... | files...]",
+    "List of strings, regular expressions or files.\nProvided files should contain a list of strings or regular expressions.\nRegular expressions should have \"\\\" escaped (eg. \"\\d\") or be inside quotes.\nGiven operands are treated as regular expressions by default."
+)
+
+OPTIONS = Table(box=None)
+OPTIONS.add_column(justify="left", style="green")
+OPTIONS.add_column(justify="left", style="cyan")
+OPTIONS.add_column(justify="left", style="yellow")
+OPTIONS.add_column(justify="left")
+OPTIONS.add_row(
+    "-s",
+    "--separator",
+    "<string>",
+    "Separator used in splitting generated sequences. \\[default:' ']"
+)
+OPTIONS.add_row(
+    "-t",
+    "--times",
+    "<int>",
+    "Number of times splitn generates sequences for each specification. Applied only for regular expressions. \\[default: 1]"
+)
+OPTIONS.add_row(
+    "",
+    "--secondary-separator",
+    "<string>",
+    "Separator used to separate outputs from different provided specifications. Use empty string for having new line. \\[default: ---]"
+)
+OPTIONS.add_row(
+    "",
+    "--as-string",
+    "",
+    "Interpret provided operands as simple strings."
+)
+OPTIONS.add_row(
+    "-p",
+    "--pattern",
+    "<regexes...>",
+    "Use this option to either generate random sequence from regular expressions without splitting, or to narrow down sequences generated from given operands to those matching provided regular expressions. \\[default: None]"
+)
+OPTIONS.add_row(
+    "-v",
+    "--version",
+    "",
+    "Show version of splitn."
+)
+OPTIONS.add_row(
+    "-h",
+    "--help",
+    "",
+    "Show this message."
+)
+
+parser = argparse.ArgumentParser(
+        prog="splitn",
+        usage="%(prog)s [options] [operands ...] [--pattern | -p <regexes> ...]",
+        add_help=False,
+        allow_abbrev=False
+    )
+parser.add_argument(
+    "operands",
+    nargs="*",
+    type=str
+)
+parser.add_argument(
+    "--separator", "-s",
+    default=" ",
+    type=str
+)
+parser.add_argument(
+    "--times", "-t",
+    default=1,
+    type=int
+)
+parser.add_argument(
+    "--secondary-separator",
+    default="---",
+    type=str
+)
+parser.add_argument(
+    "--as-string",
+    action="store_true"
+)
+parser.add_argument(
+    "--pattern", "-p",
+    nargs="*",
+    default=None,
+    type=str
+)
+parser.add_argument(
+    "--version", "-v",
+    action="version",
+    version="%(prog)s " + VERSION
+)
+parser.add_argument(
+    "--help", "-h",
+    action="store_true"
+)
+
+console = Console()
 
 @logger.catch
 def generate_output(
@@ -28,9 +159,9 @@ def generate_output(
                 sequence = random_sequence(operand)
                 generate_split_sequences(sequence, separator, patterns)
                 if counter < times - 1:
-                    print()
+                    console.print()
     except Exception as e:
-        raise typer.Abort(f"Program aborted with exception: {e}.")
+        parser.error(f"Program aborted with exception: {e}.")
 
 @logger.catch
 def generate_split_sequences(
@@ -46,7 +177,7 @@ def generate_split_sequences(
                 if printable:
                     break
         if printable:
-            print(split_sequence)
+            console.print(split_sequence)
 
 @logger.catch
 def detect_string(
@@ -57,65 +188,63 @@ def detect_string(
     except:
         return False
 
-app = typer.Typer()
+@logger.catch
+def main(args=argv[1:]) -> None:
+    args = parser.parse_args(args)
 
-@app.command()
-def main(
-    operands: Annotated[Optional[list[str]], typer.Argument(
-        help="""
-        List of strings, regular expressions or files.
-
-        Provided files should contain a list of strings or regular expressions.
-
-        Regular expressions should have "\\" escaped (eg. "\\\\d") or be inside quotes.
-
-        Given operands are treated as regular expressions by default.
-        """
-    )] = None,
-    separator: Annotated[str, typer.Option(
-        "--separator", "-s",
-        help="Separator used in splitting generated sequences."
-    )] = " ",
-    times: Annotated[int, typer.Option(
-        "--times", "-t",
-        help="Number of times splitn generates sequences for each specification. Applied only for regular expressions."
-    )] = 1,
-    secondary_separator: Annotated[str, typer.Option(
-        "--secondary-separator",
-        help="Separator used to separate outputs from different provided specifications. Use empty string for having new line."
-    )] = "---",
-    as_string: Annotated[bool, typer.Option(
-        "--as-string",
-        help="Interpret provided operands as simple strings."
-    )] = False,
-    patterns: Annotated[list[str], typer.Option(
-        "--pattern", "-p",
-        help="Use this option to either generate random sequence from regular expressions without splitting, or to narrow down sequences generated from given operands to those matching provided regular expressions.",
-    )] = None
-) -> None:
-    if operands:
+    if args.help:
+        console.print(
+            DESCRIPTION,
+            USAGE,
+            "\n",
+            Panel(
+                ARGUMENTS,
+                title="Arguments",
+                title_align="left"
+            ),
+            Panel(
+                OPTIONS,
+                title="Options",
+                title_align="left"
+            )
+        )
+        parser.exit()
+    elif args.operands:
+        operands: list[str] | None = args.operands
         for operand, counter in zip(operands, range(len(operands), 0, -1)):
             if path.exists(operand):
                 with open(operand) as file:
                     lines = file.readlines()
                     for line, line_counter in zip(lines, range(len(lines), 0, -1)):
-                        generate_output(line.strip(), separator, times, as_string, patterns)
+                        generate_output(line.strip(), args.separator, args.times, args.as_string, args.pattern)
                         if line_counter > 1:
-                            print(secondary_separator)
+                            console.print(args.secondary_separator)
             else:
-                generate_output(operand, separator, times, as_string, patterns)
+                generate_output(operand, args.separator, args.times, args.as_string, args.pattern)
             if counter > 1:
-                print(secondary_separator)
-    elif patterns:
+                console.print(args.secondary_separator)
+        parser.exit()
+    elif args.pattern:
+        patterns: list[str] | None = args.pattern
         for pattern, counter in zip(patterns, range(len(patterns), 0, -1)):
-            for time in range(times):
-                print(random_sequence(pattern))
+            for time in range(args.times):
+                console.print(random_sequence(pattern))
         if counter > 1:
-            print(secondary_separator)
+            console.print(args.secondary_separator)
+        parser.exit()
     else:
-        print("Missing argument 'OPERANDS...' or an option.")
-        raise typer.Exit(code=1)
-
+        console.print(
+            DESCRIPTION,
+            USAGE,
+            "\n",
+            Panel(
+                EXAMPLE,
+                title="Example",
+                title_align="left"
+            ),
+            EPILOG
+        )
+        parser.exit(1)
 
 if __name__ == "__main__":
-    app()
+    main()
--- tests/test_splitn.py
+++ tests/test_splitn.py
@@ -1,57 +1,68 @@
-from unittest import result
-from typer.testing import CliRunner
+import pytest
 from random import seed
-
-from splitn.main import app
-
-runner = CliRunner()
+from splitn.main import main
 
 INPUT_1 = "123"
 OUTPUT_1 = "123\n1 23\n12 3\n1 2 3\n"
 
-INPUT_2 = "\\\\d{2}"
+INPUT_2 = "\\d{2}"
 OUTPUT_2 = "60\n6 0\n"
 
-INPUT_3 = "\\d{2}"
+SECONDARY_SEPARATOR = "---\n"
 OUTPUT_3 = "87\n8 7\n"
 
 SEPARATOR = "-"
-OUTPUT_SEPARATOR = "123\n1-23\n12-3\n1-2-3\n"
+OUTPUT_4 = "123\n1-23\n12-3\n1-2-3\n"
 
 PATTERN = "\\d{2} \\d"
-OUTPUT_4 = "47 5\n"
-OUTPUT_5 = "12 3\n"
+OUTPUT_5 = "47 5\n"
 
-def test_app() -> None:
-    seed(0)
-    result = runner.invoke(app, INPUT_1)
-    assert result.exit_code == 0
-    assert result.stdout == OUTPUT_1
+OUTPUT_6 = "12 3\n"
 
-def test_regex() -> None:
+def test_app(capsys) -> None:
     seed(0)
-    result = runner.invoke(app, str(INPUT_2))
-    assert result.stdout == OUTPUT_2
+    with pytest.raises(SystemExit) as e:
+        main([INPUT_1])
+    assert e.type == SystemExit
+    assert e.value.code == 0
+    captured = capsys.readouterr()
+    assert captured.out == OUTPUT_1
+
+def test_regex(capsys) -> None:
+    seed(0)
+    with pytest.raises(SystemExit) as e:
+        main([INPUT_2])
+    captured = capsys.readouterr()
+    assert captured.out == OUTPUT_2
     
-def test_two_operands() -> None:
+def test_two_operands(capsys) -> None:
     seed(0)
-    result = runner.invoke(app, [INPUT_1, INPUT_3])
-    assert result.stdout == OUTPUT_1 + "---\n" + OUTPUT_2
+    with pytest.raises(SystemExit) as e:
+        main([INPUT_1, INPUT_2])
+    captured = capsys.readouterr()
+    assert captured.out == OUTPUT_1 + SECONDARY_SEPARATOR + OUTPUT_2
 
-def test_separator() -> None:
+def test_separator(capsys) -> None:
+    with pytest.raises(SystemExit) as e:
+        main(["--separator", SEPARATOR, INPUT_1])
+    captured = capsys.readouterr()
+    assert captured.out == OUTPUT_4
+
+def test_times(capsys) -> None:
     seed(0)
-    result = runner.invoke(app, ["--separator", SEPARATOR, INPUT_1])
-    assert result.stdout == OUTPUT_SEPARATOR
+    with pytest.raises(SystemExit) as e:
+        main(["--times", "2", INPUT_2])
+    captured = capsys.readouterr()
+    assert captured.out == OUTPUT_2 + "\n" + OUTPUT_3
 
-def test_times() -> None:
-    seed(0)
-    result = runner.invoke(app, ["--times", "2", INPUT_3])
-    assert result.stdout == OUTPUT_2 + "\n" + OUTPUT_3
+def test_pattern(capsys) -> None:
+    with pytest.raises(SystemExit) as e:
+        main(["--pattern", PATTERN])
+    captured = capsys.readouterr()
+    assert captured.out == OUTPUT_5
 
-def test_pattern() -> None:
-    result = runner.invoke(app, ["--pattern", PATTERN])
-    assert result.stdout == OUTPUT_4
-
-def test_pattern_with_operand() -> None:
-    result = runner.invoke(app, [INPUT_1, "--pattern", PATTERN])
-    assert result.stdout == OUTPUT_5
+def test_pattern_with_operand(capsys) -> None:
+    with pytest.raises(SystemExit) as e:
+        main([INPUT_1, "--pattern", PATTERN])
+    captured = capsys.readouterr()
+    assert captured.out == OUTPUT_6
